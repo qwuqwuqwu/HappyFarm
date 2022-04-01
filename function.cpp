f931ff76ee2e1eadf156f6ceed894cd1cbeb3fb9
qwuqwuqwu <qwuqwuqwu@gmail.com>
2022-04-01 18:01:00 +0800

    add: insert, delete by row major

--- function.cpp
+++ function.cpp
@@ -13,8 +13,15 @@ fence( fence )
 template <class T>
 T Node< T >::compare( T x,T y )
 {
-
+    if( this->x == x && this->y == y) {
+        return ( T )( true ); // true
+    }
+    else{
+        return ( T )( false ); // false
+    }
 }
+
+template class Node< long long >;
 
 //============================================
 template < class T >
@@ -25,51 +32,100 @@ length( 0 )
     // do nothing
 }
 
-template class Node< int >;
-
 template < class T >
 void LinkedList< T >::insert( T x, T y, T fence )
 {
-    Node< int > NewNode( x, y, fence );
-    Node< int > *pTempNode = nullptr;
+    Node< long long > *pNewNode = new Node< long long >( x, y, fence );
+    pNewNode->next = nullptr;
+    Node< long long > *pTempNode = nullptr;
+    Node< long long > *pLastNode = nullptr;
     // exist first
-    if( first ) {
+    if( first != nullptr ) {
         pTempNode = first;
         while( pTempNode != nullptr ) {
-            // tail node is found
-            if( pTempNode->next == nullptr ) {
-                pTempNode->next = NewNode;
+            if( pTempNode->x == x ) {
+                if( pTempNode->y == y ) {
+                    // check fence
+                    if( pTempNode->fence == 1 || fence == 1 ) {
+                        pTempNode->fence = 1;
+                        return;
+                    }
+                }
+                else if( pTempNode->y > y ) {
+                    // add between last and temp
+                    pNewNode->next = pTempNode;
+                    pLastNode->next = pNewNode;
+                    length += 1;
+                    return;
+                }
+                else {
+                    // read next node
+                }
             }
-            // exist next node
-            else {
-                pTempNode = pTempNode->next;
+            else if( pTempNode-> x > x ) {
+                // add betwee last and temp
+                pNewNode->next = pTempNode;
+                pLastNode->next = pNewNode;
+                length += 1;
+                return;
             }
+
+            // read next node
+            pLastNode = pTempNode;
+            pTempNode = pTempNode->next;
+            // // tail node is found
+            // if( pTempNode->next == nullptr ) {
+            //     pTempNode->next = pNewNode;
+            //     legnth += 1;
+            //     return;
+            // }
+            // // exist next node
+            // else {
+            //     pTempNode = pTempNode->next;
+            // }
+        }
+
+        if( pTempNode == nullptr ) {
+            // add to tail
+            pLastNode->next = pNewNode;
+            length += 1;
+            return;
         }
     }
     // no node in linkedlist
     {
-        first = NewNode;
+        first = pNewNode;
+        length += 1;
     }
 }
 
 template < class T >
 void LinkedList< T >::deletion( T x, T y )
 {
-    Node< int > *pTempNode = nullptr;
-    Node< int > *pNextNode = nullptr;
-    Node< int > *pLastNode = nullptr;
+    Node< long long > *pTempNode = nullptr;
+    Node< long long > *pNextNode = nullptr;
+    Node< long long > *pLastNode = nullptr;
     // exist first
-    if( first ) {
+    if( first != nullptr ) {
         pTempNode = first;
         while( pTempNode != nullptr ) {
             // target node is found
             if( pTempNode->compare( x, y ) ) {
-                pNextNode = pTempNode->next;
-                pLastNode->next = pTempNode;
+                // check fence
+                if( pTempNode->fence == 1 ) {
+                    return;
+                }
+                pLastNode->next = pTempNode->next;
+                delete pTempNode;
+                pTempNode = NULL;
+                length -= 1;
                 return;
             }
             // exam next node
             else {
+                if( pTempNode->x > x ) {
+                    return;
+                }
                 pLastNode = pTempNode;
                 pTempNode = pTempNode->next;
             }
@@ -77,7 +133,7 @@ void LinkedList< T >::deletion( T x, T y )
     }
     // no node in linkedlist
     {
-        assert( false );
+        //assert( false );
         return;
     }
 }
@@ -85,20 +141,41 @@ void LinkedList< T >::deletion( T x, T y )
 template < class T >
 void LinkedList< T >::show()
 {
-
+    // print all node
+    Node< long long > *pTempNode;
+    pTempNode = first;
+    while( pTempNode != nullptr ) {
+        if( pTempNode->next == nullptr ) {
+            printf( "(%lld,%lld)\n", pTempNode->x, pTempNode->y );
+            return;
+        }
+        else {
+            printf( "(%lld,%lld)\n", pTempNode->x, pTempNode->y );
+            pTempNode = pTempNode->next;
+        }
+    }
 }
 
-template class LinkedList< int >;
+template class LinkedList< long long >;
+
+LinkedList< long long > g_Plant;
 
 // =========================
 void plant(long long x,long long y,int withFence)
 {
-
+    // insert by row, col
+    g_Plant.insert( x, y, withFence );
 }
 
 void thief(long long x,long long y)
 {
-
+    long long TargetPos[ 5 ][ 2 ] = { { x, y - 1 }, { x - 1, y }, { x, y }, { x + 1, y }, { x, y + 1 } };
+    for( int i = 0; i < 5; i++ ) {
+        if( TargetPos[ i ][ 0 ] >= -1e10 && TargetPos[ i ][ 0 ] <= 1e10 &&
+            TargetPos[ i ][ 1 ] >= -1e10 && TargetPos[ i ][ 1 ] <= 1e10 ) {
+                g_Plant.deletion( TargetPos[ i ][ 0 ], TargetPos[ i ][ 1 ] );
+        }
+    }
 }
 
 void superThief(long long x,long long y)
@@ -108,5 +185,6 @@ void superThief(long long x,long long y)
 
 void display()
 {
-
+    printf( "DISPLAY\n" );
+    g_Plant.show();
 }
